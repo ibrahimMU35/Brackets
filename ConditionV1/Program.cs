578286b4678d0424550b16bea3ad00449e764d1b
ibrahim <ibrahimmu3533@gmail.com>
2024-03-24 17:01:53 +0100

    update

---
+++ ConditionV1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int age;
+int height;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Please input your age:");
+age= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("please input your height(cm):");
+height= Convert.ToInt32(Console.ReadLine());
+
+if (age >= 18 && height >= 160 ){
+    Console.WriteLine("you can enter to the rollercoster");
+
+}else 
+{
+    Console.WriteLine("these requirements are not enough to enter the rollercoster");
+}
+
+Console.ReadKey();//wait befor closing
