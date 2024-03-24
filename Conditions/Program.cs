1003cdf92210a016713cd5b69a633da381ce6614
ibrahim <ibrahimmu3533@gmail.com>
2024-03-24 16:07:49 +0100

    update

---
+++ Conditions/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome , Tickets are 5$!. Please insert cash.");
+int cash = Convert.ToInt32(Console.ReadLine()); 
+if (cash < 5){
+    Console.WriteLine("you are not enough money");
+
+}
+
+else if (cash == 5){
+Console.WriteLine("here is your ticket");
+}
+
+else {
+    int change = cash - 5 ;
+    Console.WriteLine("here is your ticket and " + change + " dollars in change");
+}
+
+
+
+
+Console.ReadKey();
