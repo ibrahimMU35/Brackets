7a489d641200e9a6240e738403e137616ea2d14c
ibrahim <ibrahimmu3533@gmail.com>
2024-03-24 11:50:33 +0100

    firstBeta

--- My Awesome Program/Program.cs
+++ My Awesome Program/Program.cs
@@ -2,5 +2,19 @@
 
 
 
+using System.Diagnostics;
+
 Console.WriteLine("Hello, World!");
+
+Console.Title="Skynet";
+Console.ForegroundColor =ConsoleColor.Green; 
+Console.WindowHeight=40;
+
+Console.WriteLine("Hello What is your name?");
+Console.ReadLine();
+
+
+Console.WriteLine("my name is rx-900. im an Ai sent from the future for destroying the world");
+Console.WriteLine("what is your favorite color ");
+
 Console.ReadKey();
--- My Awesome Program/obj/Debug/net8.0/My Awesome Program.AssemblyInfo.cs
+++ My Awesome Program/obj/Debug/net8.0/My Awesome Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Awesome Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ead40959b707e06140b668823dc084cfe00d057")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35959aecf0bf8617dafd92f7faa56fc87c7d5442")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Awesome Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Awesome Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
