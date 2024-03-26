520fb2e3e6c9202853cc951b4e0a2951e7510a46
ibrahim <ibrahimmu3533@gmail.com>
2024-03-26 18:12:48 +0100

    update

---
+++ Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+for(int i=0; i<=10; i++){
+    Console.WriteLine(i);
+}
+
+for(double j=10; j>=0; j-- )
+{
+    Console.WriteLine(j);
+} 
+// wait for looading
+Console.ReadKey();
