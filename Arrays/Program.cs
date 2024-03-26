dc1eb85ecc094d3b60f8f327bec9c7542bb3ef6d
ibrahim <ibrahimmu3533@gmail.com>
2024-03-26 19:29:46 +0100

    update

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -2,6 +2,8 @@
 string[] movies= {"lord of the rings", "Gladiator", "Upgrate", "Blade runner"};
 
 for (int i=0; i<movies.Length; i++){
-    Console.WriteLine(movies[i]);
+    //Console.WriteLine(movies[i]);
+int rank = i+1;
+Console.WriteLine(rank + ". "+movies[i]);
 }
 Console.ReadKey();
