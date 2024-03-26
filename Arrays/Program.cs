2a6d314e299c680d63d92b3fee9eb0face90e24e
ibrahim <ibrahimmu3533@gmail.com>
2024-03-26 19:27:36 +0100

    upgrade

---
+++ Arrays/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] movies= {"lord of the rings", "Gladiator", "Upgrate", "Blade runner"};
+
+for (int i=0; i<movies.Length; i++){
+    Console.WriteLine(movies[i]);
+}
+Console.ReadKey();
